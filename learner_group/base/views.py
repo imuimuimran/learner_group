ce8e2e9c0073e12bf4e6991d02aa694a70eb460e
Mohd. Imran Hossain <hossainmohdimran@gmail.com>
2024-04-30 20:47:52 +0600

    create dictionary in views.py and apply it to home.html for test

--- learner_group/base/views.py
+++ learner_group/base/views.py
@@ -2,8 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 
+rooms = [
+    {'id': 1, 'name': 'Python Programmers'},
+    {'id': 2, 'name': 'Java Programmers'},
+    {'id': 3, 'name': 'C++ Programmers'},
+]
+
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'rooms': rooms})
 
 def room(request):
     return render(request, 'room.html')
