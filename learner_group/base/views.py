f6e99ee014edb952c07479a8de729daaaf39195b
Mohd. Imran Hossain <hossainmohdimran@gmail.com>
2024-05-01 16:58:20 +0600

    room added with home page dinamicly

--- learner_group/base/views.py
+++ learner_group/base/views.py
@@ -1,22 +1,21 @@
 from django.shortcuts import render
+from .models import Room
 
 # Create your views here.
 
-rooms = [
-    {'id': 1, 'name': 'Python Programmers'},
-    {'id': 2, 'name': 'Java Programmers'},
-    {'id': 3, 'name': 'C++ Programmers'},
-]
+# rooms = [
+#     {'id': 1, 'name': 'Python Programmers'},
+#     {'id': 2, 'name': 'Java Programmers'},
+#     {'id': 3, 'name': 'C++ Programmers'},
+# ]
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
-        context = {'room': room}
+    room = Room.objects.get(id=pk)
+    context = {'room': room}
     
     return render(request, 'base/room.html', context)
